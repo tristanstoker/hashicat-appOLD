851bd4fe01d3098c33d88063161187a331a67681
tristanstoker <tristanstoker@gmail.com>
2023-09-08 16:44:28 +0000

    Added Backup Storage private module

--- main.tf
+++ main.tf
@@ -201,8 +201,8 @@ resource "null_resource" "configure-cat-app" {
     }
   }
 }
-module "azure-backup" {
-  source  = "ravensorb/azure-backup/azurerm"
+module "backupstorage" {
+  source = "app.terraform.io/workshop110/azure-backup/azurerm"
   version = "1.0.2"
-  resource_group_name = "${var.prefix}-public"
+  resource_group_name = "${var.prefix}-private"
 }
